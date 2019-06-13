5a112e32a8966a88e7bec0c83cca34039e844608
limheeeun <45805746+limheeeun@users.noreply.github.com>
2019-06-13 12:44:39 +0900

    Add files via upload

--- r2/top.js
+++ r2/top.js
@@ -1,15 +1,14 @@
-$( document ).ready( function() {  
-    $("#upBtn").css("display", "none");
-    $(window).scroll( function() {
+$(document).ready(function() {  
+    $(window).scroll(function(event) {
         var scTop = $(window).scrollTop();
-      if ( scTop >= 50 ) {
+        if (scTop >= 200) {
         $("#upBtn").fadeIn();
       } else {
-        $( "#upBtn" ).fadeOut();
+        $("#upBtn").fadeOut();
       }
     });
-    $( "#upBtn" ).click( function() {
-      $( 'html, body' ).animate( { scrollTop : 0 }, 800 );
+    $("#upBtn").click(function() {
+      $("html, body").animate({scrollTop : 0}, 800);
       return false;
     });
-  });
+});
